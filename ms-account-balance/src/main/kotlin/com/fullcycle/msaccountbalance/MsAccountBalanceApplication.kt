2a1b24a5115e5221dc8d0d6210fa4bd71e3b5337
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-05-09 20:38:56 -0300

    feat: bootstrap ms-account-balance project

---
+++ ms-account-balance/src/main/kotlin/com/fullcycle/msaccountbalance/MsAccountBalanceApplication.kt
@@ -0,0 +1,11 @@
+package com.fullcycle.msaccountbalance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsAccountBalanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsAccountBalanceApplication>(*args)
+}
---
+++ ms-account-balance/src/test/kotlin/com/fullcycle/msaccountbalance/MsAccountBalanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fullcycle.msaccountbalance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsAccountBalanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
