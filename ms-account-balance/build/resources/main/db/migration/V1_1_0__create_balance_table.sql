e9932f11d434e6f7f60a7f7bac39cf21e8d5967e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-05-17 08:36:48 -0300

    fix: include gitignore

--- ms-account-balance/build/resources/main/db/migration/V1_1_0__create_balance_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE balance (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id VARCHAR(36) NOT NULL,
-    value DOUBLE NOT NULL,
-    UNIQUE INDEX balance_accountId(account_id)
-)
